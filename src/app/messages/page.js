2920ee3af13c18537c67e3e5f9fa60c0d87e9525
LavkushUpadhyay <lavkushupadhyay0@gmail.com>
2024-01-23 21:54:44 +0530

    code upload 23 jan 24

--- src/app/messages/page.js
+++ src/app/messages/page.js
@@ -22,65 +22,63 @@ function Messages() {
       <section className="mt-5">
         <div className="container">
           {list?.Team?.length > 0
-            ? list?.Team?.filter((it) => it?._id === "Board of Member").map(
-                (tm, i) => (
-                  <>
-                    <div
-                      className="row align-items-center justify-content-center"
-                      key={i}
-                    >
-                      {/* <div className="col-sm-12 col-md-12 col-lg-12 mb-4 text-center">
+            ? list?.Team?.map((tm, i) => (
+                <>
+                  <div
+                    className="row align-items-center justify-content-center"
+                    key={i}
+                  >
+                    {/* <div className="col-sm-12 col-md-12 col-lg-12 mb-4 text-center">
                         <h3>{tm?._id}</h3>
                       </div> */}
-                      {tm?.teams?.map((item, ii) => (
+                    {tm?.teams?.map((item, ii) => (
+                      <div
+                        className="col-sm-12 col-md-12 col-lg-12 mb-4"
+                        key={ii}
+                      >
                         <div
-                          className="col-sm-6 col-md-4 col-lg-4 mb-4"
-                          key={ii}
+                          className="bg-white shadow rounded p-3"
+                          onClick={() => {
+                            setdescription(item);
+                            handleShow();
+                          }}
+                          role="button"
                         >
-                          <div
-                            className="bg-white shadow rounded p-3"
-                            onClick={() => {
-                              setdescription(item);
-                              handleShow();
-                            }}
-                            role="button"
-                          >
-                            <div className="d-grid text-center gap-3">
-                              <Image
-                                src={
-                                  item?.profilePicture?.includes("http")
-                                    ? item?.profilePicture
-                                    : "/assets/img/dummyImage.png"
-                                }
-                                alt=""
-                                quality={100}
-                                priority
-                                width={80}
-                                height={80}
-                                className="rounded-circle ob-cover m-auto"
-                              />
-                              <div>
-                                <h5 className="text-capitalize">
-                                  {item?.fullName}
-                                </h5>
-                                <h6>
-                                  <i>{item?.designation}</i>
-                                </h6>
-                              </div>
+                          <div className="d-grid text-center gap-3">
+                            <Image
+                              src={
+                                item?.profilePicture?.includes("http")
+                                  ? item?.profilePicture
+                                  : "/assets/img/dummyImage.png"
+                              }
+                              alt=""
+                              quality={100}
+                              priority
+                              width={80}
+                              height={80}
+                              className="rounded-circle ob-cover m-auto"
+                            />
+                            <div>
+                              <h5 className="text-capitalize">
+                                {item?.fullName}
+                              </h5>
+                              <h6>
+                                <i>{item?.designation}</i>
+                              </h6>
                             </div>
-                            <div
-                              className="line2"
-                              dangerouslySetInnerHTML={{
-                                __html: item?.message,
-                              }}
-                            ></div>
                           </div>
+                          <div
+                            className="line2"
+                            dangerouslySetInnerHTML={{
+                              __html: item?.message,
+                            }}
+                          ></div>
                         </div>
-                      ))}
-                    </div>
-                  </>
-                )
-              )
+                      </div>
+                    ))}
+                  </div>
+                </>
+              ))
             : null}
           {/* {list?.Team?.length > 0 ? (
             <>
