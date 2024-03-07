43e3ef4057d7de113934464d8063dc69e53ce1df
LavkushUpadhyay <lavkushupadhyay0@gmail.com>
2024-03-07 23:38:23 +0530

    code upload 07 Mar 24

--- src/components/provider/Provider.jsx
+++ src/components/provider/Provider.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Menu from "../menu/Menu";
 import Footer from "../footer/Footer";
 import Bredthcred from "@/components/bredthcred/Bredthcred";
+import { usePathname } from "next/navigation";
 
 function Provider(props) {
+  const pathname = usePathname();
   return (
     <>
       <Menu />
-      <section className="bgColor">
+      <section className={pathname !== "/" && "bgColor"}>
         <Bredthcred />
         <div className="min-vh-50">{props.children}</div>
       </section>
